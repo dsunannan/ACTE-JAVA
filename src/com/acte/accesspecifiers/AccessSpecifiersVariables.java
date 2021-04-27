daa62bf22c5626689632dc5cf44e0a1f63528212
Sundarakannan <dsunkannan@gmail.com>
2021-04-27 09:20:33 +0530

    Java Primitive Data Types & Flow controls
    Date : 27/04/2021
    Trainer : SundaraKannan D
    Session:3

---
+++ src/com/acte/accesspecifiers/AccessSpecifiersVariables.java
@@ -0,0 +1,12 @@
+package com.acte.accesspecifiers;
+
+public class AccessSpecifiersVariables {
+	/*
+	 * private,protected and public
+	 * If you don't specify any access specifiers then default
+	 */
+	private int i; //most restrictive
+	protected int j;// somewhat restrivtive class + extended class 
+	public int k; // Fully acceesible
+	 int  m; 
+}
